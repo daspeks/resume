ae61e8efd5cd1712bc17a54ff31f875d7779d073
Parth <daspeks@gmail.com>
2020-04-30 15:54:49 -0600

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-62d0a646a57959cba758.js"
+    "url": "webpack-runtime-ed1e7d1ceb4e1ee00fd9.js"
   },
   {
     "url": "framework-37b45856124a722f0eb5.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e579bdb228d62c20cd6a32fb981dbbd4"
+    "revision": "600563d85553ede3a8e16c1e133a690e"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b1462017a2c34a95e2b4.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "5c68d0dad5b4b986a97b82785f88e0c5"
+    "revision": "f7ee69a2a220e2eb0ea0eaaf825d707c"
   },
   {
     "url": "manifest.webmanifest",
